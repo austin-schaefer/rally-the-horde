348a407c60dfa62a3d8933eee7dabeed89eb227b
Austin Schaefer <55203603+austin-schaefer@users.noreply.github.com>
2023-03-11 23:23:00 -0800

    Clean up some embarassing count mistakes

--- main.py
+++ main.py
@@ -5,35 +5,31 @@ def count(iteratble):
     return sum(1 for _ in iterable)
 
 # Create the deck
-deck=[]
+deck = []
 for _ in range(17):
     deck.append("L")
 for _ in range(23):
     deck.append("S")
 random.shuffle(deck)
 
-# Variable for tracking which card we're on
-current_card = 0
-
 # Draw cards into "hand" until it has 7 lands
 hand=[]
 lands = 0
 while lands < 7:
-    hand.append(deck[current_card])
-    if deck[current_card] == "L":
+    hand.append(deck[0])
+    if deck[0] == "L":
         lands += 1
-    deck.pop(current_card)
-    current_card += 1
+    deck.pop(0)
 
 # Debugging output
 print("Initial hand:", hand)
 print("Initial deck:", deck)
 print("\n")
 
-
 # Tests top card of deck to see if it's a spell
 warriors = 0
 end_the_rally = False
+decked_ourselves = False
 def rally_test():
     global warriors
     if deck[0] == "S":
@@ -51,5 +47,10 @@ while end_the_rally == False:
     print("Current deck:", deck)
     print("  Warrior count:", warriors)
     print("  Cards left in deck:", len(deck))
-    # if len(deck) <= 3:
-    #     game_over = True
+    if len(deck) <= 3:
+        decked_ourselves = True
+        end_the_rally = True
+
+print("Final warrior count:", warriors)
+print("Final deck count:", len(deck))
+print("Did we deck ourselves:", decked_ourselves)
