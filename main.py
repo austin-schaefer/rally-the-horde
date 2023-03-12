f769dff63e9f152390b0d3641893e5e7c7e80af5
Austin Schaefer <55203603+austin-schaefer@users.noreply.github.com>
2023-03-11 23:48:01 -0800

    Handle decking ourselves

--- main.py
+++ main.py
@@ -26,10 +26,12 @@ print("Initial hand:", hand)
 print("Initial deck:", deck)
 print("\n")
 
-# Tests top card of deck to see if it's a spell
+# variables for rally the horde functions
 warriors = 0
 end_the_rally = False
 decked_ourselves = False
+
+# Tests top card of deck to see if it's a spell
 def rally_test():
     global warriors
     if deck[0] == "S":
@@ -38,18 +40,23 @@ def rally_test():
 
 # Cast Rally the Horde
 while end_the_rally == False:
-    # Test if first card is spell
-    rally_test()
-    rally_test()
-    if deck[0] == "L":
+    # If there are less than 3 cards at the start, we decked ourselves
+    if len(deck) <= 3:
+        while len(deck) > 0:
+            rally_test()
+        decked_ourselves = True
         end_the_rally = True
-    rally_test()
+    # Run an iteration
+    if len(deck) > 3:
+        rally_test()
+        rally_test()
+        if deck[0] == "L":
+            end_the_rally = True
+        rally_test()
+    # Print some debug stuff
     print("Current deck:", deck)
     print("  Warrior count:", warriors)
     print("  Cards left in deck:", len(deck))
-    if len(deck) <= 3:
-        decked_ourselves = True
-        end_the_rally = True
 
 print("Final warrior count:", warriors)
 print("Final deck count:", len(deck))
